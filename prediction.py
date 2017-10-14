c67301478f04ca7ae7d62d60010a2a82f59964a3
Brendan Wong <brendanwong@Brendan-Wongs-MacBook-Air.local>
2017-10-14 10:54:09 -0700

    created unique company list for seed

--- prediction.py
+++ prediction.py
@@ -1,44 +1,21 @@
 import pandas as pd
 
-def funding_count(df, round_type):
-    """
-    Input: Dataframe, name of funding_round_type
-    Output: Integer, unique company funding round count
-    """
-    return (df[['company_name', 'funding_round_type']][(df['funding_round_type'] == round_type)]).groupby('company_name').count()
+df = pd.read_csv('/Users/brendanwong/galvanize/Capstone/crunchbase-data/investments.csv')
 
-def funding_count_det(df, round_type):
-    """
-    Similar to funding_count function except it gives the detailed count by round type (e.g. A, B, C)
-    Input: Dataframe, name of funding_round_type
-    Output: Integer, count for each funding_round_code
-    """
-    funding_round_code_list = list(df[(df['funding_round_type'] == round_type)]['funding_round_code'].unique())
+## Companies with seed
+seeded_array = (df[(df['funding_round_type'] == 'seed')]['company_name'].unique())
 
-    for code in funding_round_code_list:
-        print code, (
-        df
-        [['company_name', 'funding_round_type', 'funding_round_code']]
-        [(df['funding_round_type'] == round_type)]
-        [(df['funding_round_code'] == code)]
-        ).groupby('company_name').count()
+## Investors post-seed
+# beyond_seed = ['']
+# investors_array = (df[(df['funding_round_type'] == 'seed')]['company_name'].unique())
 
-# # Companies with Series A
-# comp_ser_a = df[['company_name', 'funding_round_type']][(df['funding_round_type'] == 'series-a')]
-# # comp_ser_a.count
 #
 # # Target column representing future rounds
 # df[['company_name', 'funding_round_type']].groupby(['company_name', 'funding_round_type']).count()
 
 if __name__ == "__main__":
-    df = pd.read_csv('/Users/brendanwong/galvanize/Capstone/crunchbase-data/investments.csv')
+    pass
 
-    # # Counts by funding_round_type
-    # funding_rounds = list(df['funding_round_type'].unique())
-    # for round_type in funding_rounds:
-    #     print round_type, funding_count(df, round_type)
-
-    funding_count_det(df, 'angel')
     # # Examining funding_round_code per funding_round_type
     # df[['company_name', 'funding_round_type', 'funding_round_code']].groupby(['company_name', 'funding_round_type', 'funding_round_code']).count()
     # df[(df['funding_round_type'] == 'seed')].head(100)
