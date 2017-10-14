55083cbb114f04545d2e632dc131c58a8782dc37
Brendan Wong <brendanwong@Brendan-Wongs-MacBook-Air.local>
2017-10-14 12:42:17 -0700

    edits to prediction file and readme for investor count by seed inve

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Objectives
 
+- Can we predict if you'll get further rounds of financing based on size of your seed investor?
+
 - Based on which investor invested in earlier financing rounds (e.g. angel, A), can we predict the probability of securing future financing?
 
 - As an investor, what is the optimal network structure for investing in startups?
@@ -24,7 +26,7 @@ _Funding types_
 - undisclosed: 3,810
 - angel: 2,508
     - A: 13
-    - B: 
+    - B: 1
 - debt_financing: 1,328
 - private_equity: 1,260
 - grant: 922
@@ -35,6 +37,33 @@ _Funding types_
 - secondary_market: 73
 - post_ipo_debt: ?
 - product_crowdfunding: ?
+
+_Investors by seed count_
+- At least 1: 30,189
+- At least 2: 12,104
+- At least 3: 7,954
+- At least 4: 6,074
+- At least 5: 5,306
+- At least 6: 4,316
+- At least 7: 3,797
+- At least 8: 3,390
+- At least 10: 2828
+- At least 15: 1,968
+- At least 20: 1,501
+- At least 25: 1,208
+- At least 50: 558
+- At least 100: 209
+- At least 150: 117
+- At least 200: 75
+- At least 250: 57
+- At least 300: 44
+- At least 350: 32
+- At least 400: 22
+- At least 500: 12
+- At least 600: 11
+- At least 700: 9
+- At least 800: 6
+
 
 # Prediction: Can investors predict future funding rounds?
 
--- prediction.py
+++ prediction.py
@@ -1,17 +1,36 @@
 import pandas as pd
 
+
 df = pd.read_csv('/Users/brendanwong/galvanize/Capstone/crunchbase-data/investments.csv')
 
 ## Companies with seed
 seeded_array = (df[(df['funding_round_type'] == 'seed')]['company_name'].unique())
+seeded_array.shape
 
-## Investors post-seed
+## Investors in seed
 # beyond_seed = ['']
-# investors_array = (df[(df['funding_round_type'] == 'seed')]['company_name'].unique())
+investors_array = (df[(df['funding_round_type'] == 'seed')]['investor_name'].unique())
+investors_array.shape
 
-#
+## Sort investors by number of investments
+investor_ct_df = (df[['investor_name', 'company_name']]).groupby('investor_name').count().sort_values('company_name', ascending=False)
+
+
 # # Target column representing future rounds
 # df[['company_name', 'funding_round_type']].groupby(['company_name', 'funding_round_type']).count()
+
+def multiple_rounds_prob(df):
+    """
+    Inputs: Dataframe, integer representing investor size
+    Bucket Sizes: {0:(0,10), 1:(11,50), 2:(51,200), 3:(201,400), 4:(401,700), 5:(700,1050)}
+
+    Outputs: Average probability of securing multiple rounds
+    """
+    bucket_dict = {0:(0,10), 1:(11,50), 2:(51,200), 3:(201,400), 4:(401,700), 5:(700,1050)}
+    company_dict =
+    for key in bucket_dict.iterkeys():
+
+
 
 if __name__ == "__main__":
     pass
