3e54f3f00302af3dd81f09b68944f251b4f2fadd
Brendan Wong <brendanwong@Brendan-Wongs-MacBook-Air.local>
2017-10-13 18:58:44 -0700

    bw edits to EDA script

--- EDA.py
+++ EDA.py
@@ -2,19 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 
+
 df = pd.read_csv('/Users/brendanwong/galvanize/Capstone/crunchbase-october-2013/crunchbase-investments.csv')
-print df.columns
-df.head()
 
 # Set the year
 df_year = df[(df['funded_year'] == 2004)]
 
 # By year: Company, Funding Round Type, Investor Amt Raised
-df_year[['company_name', 'funding_round_type', 'investor_name', 'raised_amount_usd']].sort_values(by='company_name')
-
-# By year: Unique Company Rounds
-df_year[['company_name', 'funding_round_type']].groupby(['company_name', 'funding_round_type']).count() #to get unique company rounds
-
+# df_year[['company_name', 'funding_round_type', 'investor_name', 'raised_amount_usd']].sort_values(by='company_name')
+#
+# # By year: Unique Company Rounds
+# df_year[['company_name', 'funding_round_type']].groupby(['company_name', 'funding_round_type']).count() #to get unique company rounds
+#
 # By year: Unique Company list (Nodes)
 company_list = list(df_year['company_name'].unique())
 investor_list = list(df_year['investor_name'].unique())
@@ -22,9 +21,15 @@ node_list = company_list + investor_list
 
 # By year: Weighted Edge list (e.g. (2, 3, {'weight': 3.1415}))
 edge_df = df_year[['company_name', 'funding_round_type', 'investor_name', 'raised_amount_usd']].sort_values(by='company_name')
-edge_df['edge_list'] = (edge_df['investor_name'] + ', ' + edge_df['company_name'])
-
-
 edge_list = []
-for i in range(len(summary)):
-    edge_list.append(tuple(edge_df['edge_list'].iloc[i:i+1]))
+for i in range(len(edge_df)):
+    edge_list.append((edge_df['investor_name'].values[i], edge_df['company_name'].values[i]))
+
+
+# Drawing node graph
+G = nx.Graph()
+G.add_nodes_from(investor_list, color='red', size=50)
+G.add_nodes_from(company_list, color='blue', size=50)
+G.add_edges_from(edge_list)
+nx.draw(G)
+plt.show()
