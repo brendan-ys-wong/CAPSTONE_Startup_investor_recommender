cb5bc8eead03db7c2aa2fcc51c5c7a1eef0f65ae
Brendan Wong <brendanwong@Brendan-Wongs-MacBook-Air.local>
2017-10-17 14:39:56 -0700

    updates to graph analysis, graphing mround rate

--- graph_analysis.py
+++ graph_analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 from collections import Counter, defaultdict
+import numpy as np
 
 pd.set_option('display.max_rows', 1000)
 df = pd.read_csv('/Users/brendanwong/galvanize/Capstone/crunchbase-data/investments.csv')
@@ -10,7 +11,6 @@ df['funded_year'] = df['funded_at'].apply(lambda x: x[0:4])
 # By year: Aggregated Node list (unique investors and companies)
 company_list = list(df['company_name'].unique())
 investor_list = list(df['investor_name'].unique())
-node_list = company_list + investor_list
 
 # By year: Edge list
 edge_df = df[['company_name', 'funding_round_type', 'investor_name', 'raised_amount_usd']].sort_values(by='company_name')
@@ -83,7 +83,8 @@ df_influence = pd.DataFrame(
     influence_table, columns=['company_name', 'degree_centrality',
     'closeness_centrality', 'betweenness_centrality', 'eigenvector_centrality'])
 df_influence['influence_rank'] = [x/20 for x in df_influence.index]
-
+company_list = list(df_2['company_name'].unique())
+df_influence['type'] = [0 if x in company_list else 1 for x in df_influence['company_name']]
 
 df_2 = df.copy().fillna(value=1)
 df_company_rounds = df_2[['company_name', 'funding_round_type', 'funding_round_code']].groupby(
@@ -96,6 +97,7 @@ freq_list = Counter(x[0] for x in company_rounds_list) # Company: # of Rounds
 invest_dict = defaultdict(set)
 for investor in df_influence['company_name']:
     invest_dict[investor] = set(df_2[(df_2['investor_name']==investor)]['company_name'])
+
 
 def mrounds_rate(df, company_rounds_list, invest_dict):
     mrounds_rate = []
@@ -113,4 +115,19 @@ def mrounds_rate(df, company_rounds_list, invest_dict):
     return mrounds_rate
 
 mround_rate = mrounds_rate(df_2, freq_list, invest_dict)
-mround_rate
+df_influence['mrounds_rate'] = mround_rate
+df_influence.head(50)
+
+def mrounds_hist(df):
+    X = []
+    for x in xrange(90):
+        X.append(df.iloc[x]['mrounds_rate'])
+    return X
+
+df_investors_only = df_influence[(df_influence['type'] == 1)].reset_index()
+df_investors_only.head(100)
+# df_investors_only.iloc[0]['mrounds_rate']
+X = mrounds_hist(df_investors_only)
+
+plt.plot(X)
+plt.show()
