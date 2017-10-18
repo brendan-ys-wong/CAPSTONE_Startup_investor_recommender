3698614ddb76a65b873240b5efb4a3c45e712c93
Brendan Wong <brendanwong@Brendan-Wongs-MacBook-Air.local>
2017-10-18 14:29:12 -0700

    updating run script for ec2

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from analysis_functions import *
 
 pd.set_option('display.max_rows', 1000)
 # df = pd.read_csv('/Users/brendanwong/galvanize/Capstone/crunchbase-data/investments.csv')
-df = pd.read_csv('/~/Capstone/crunchbase-data/investments.csv')
+df = pd.read_csv('/home/ubuntu/Capstone/crunchbase-data/investments.csv')
 df = df_preprocessing(df)
 df.head()
 
