3720a7d3e1445325936c79407e0df44d547f0cc3
Brendan Wong <brendanwong@Brendan-Wongs-MacBook-Air.local>
2017-10-22 12:24:10 -0700

    updates to readme project overview, some organization clean-up in model code

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # Summary
 This repository is a current work-in-progress, please excuse the mess.
 
-This project will build a recommender system to recommend to start-ups
-which investors are likely to be interested in investing in their next
-round of financing.
+The goal of this model is to help recommend to start-up companies which investors
+might be interested in investing in their companies. With over twenty-thousand
+active investors in our dataset, most of whom are small-investors
+investing in less than 3 companies, we run into the problem where it would be difficult
+and also undesirable to be trying to predict which of these small-time investors
+might invest, since the probability that any single small-time investors will invest is small,
+and also because the benefit a smaller investor will invest in the start-up is also relatively small. Therefore, the intention of this model is to try and identify which of the larger investors is likely to be interested in the company based on that investors previous investments.
+There are around 300 investors that have at least 50 investments, our goal will be to attempt
+to identify which of these large investors are likely to invest. The benefit to the start-up is they have a smaller list of targeted investors to chase, and if they are able to secure one of these largest investors as a lead investor, it will be much easier to secure other smaller investors
+to complete the fundraising round.
+
+
 
 
 # Exploratory Data Analysis
 Statistics from exploring the data set
+
+_Interaction Data_
+- Unique funding transactions: 111,000
+    - Unique companies: 15,000
+    - Unique investors: 20,000
+    - Companies average 7.4 investors, investors average 5.5 companies
+    - Around 300 investors with over 50 investments
+
 
 _Funding types_
 - venture: 24,264
--- run_similarity.py
+++ run_similarity.py
@@ -1,4 +1,5 @@
 import pandas as pd
+pd.set_option('display.max_rows', 800)
 import matplotlib.pyplot as plt
 import networkx as nx
 from collections import Counter, defaultdict
@@ -7,55 +8,42 @@ from analysis_functions_similarity import *
 import graphlab
 from graphlab.toolkits.cross_validation import cross_val_score, KFold
 
-
-
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-pd.set_option('display.max_rows', 800)
+# # Data ETL Pipeline
 df = pd.read_csv('/Users/brendanwong/galvanize/Capstone/crunchbase-data/investments.csv')
 df = df_preprocessing(df)
-# df.head(20)
-
 # Hold-out data is from the last 6 months of 2015
 observation_data = df[(~df['funded_year_month'].isin(['2015-07', '2015-08', '2015-09', '2015-10', '2015-11', '2015-12']))]
-# observation_data.head()
-
-
-# I dropped user-item interaction data when the company had less than 2 investors, due to the the large number of users for which this applied.
+# I dropped user-item interaction data when the company had less than 2 investors,
+# due to the the large number of users for which this applied.
 # This sparse interaction data would be expected to hurt the performance of my recommendation model.
 mask = observation_data.groupby('company_name')['investor_name'].count() >2
-# mask[mask]
 observation_data = observation_data[observation_data['company_name'].isin(mask[mask].index)]
-# observation_data.head()
-len(observation_data['company_name'].unique())
-
-# For investors as the item
-observation_data.head()
 interaction_data = observation_data[['company_name', 'investor_name']]
-# interaction_data.head(20)
+interaction_data.groupby('investor_name').count().sort_values('company_name', ascending=False)
 interaction_data.to_csv("interaction_data.csv")
 
-# For companies as the item
-# interaction_data = observation_data[['investor_name', 'company_name']]
-# interaction_data.head(20)
-# interaction_data.to_csv("interaction_data.csv")
-
+# # Model 1: Only using interaction data, item-similarity model, Jaccard difference
 sf = graphlab.SFrame.read_csv("interaction_data.csv")
 m1 = graphlab.recommender.item_similarity_recommender.create(observation_data=sf, user_id = 'company_name', item_id = 'investor_name')
 # m1.list_fields()
-m1.get_current_options()
+# m1.get_current_options()
 # m1.get_similar_items()
-m1.evaluate(sf, exclude_known_for_precision_recall=False)
-
-m1.recommend(users=['23andMe'])
-
-
+# m1.evaluate(sf, exclude_known_for_precision_recall=False)
+# m1.recommend(users=['23andMe'])
 folds = KFold(sf, num_folds=5)
 params = {'user_id':'company_name', 'item_id':'investor_name'}
 job = cross_val_score(folds, graphlab.recommender.item_similarity_recommender.create, params)
 job.get_results()
+
+
+
+
+
+
 observation_data.head()
 observation_data[(observation_data['company_country_code'] == 1)]
 observation_data['company_country_code'].unique()
