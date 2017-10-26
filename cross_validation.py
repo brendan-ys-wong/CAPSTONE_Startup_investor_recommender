811e038edc802da18658fd5bf47c403c9e56650b
Brendan Wong <brendanwong@Brendan-Wongs-MacBook-Air.local>
2017-10-25 17:54:42 -0700

    updates to code for corrected baseline bugs

---
+++ cross_validation.py
@@ -0,0 +1,48 @@
+# # Baseline
+# - Seed & Venture Only
+# - No 2H of 2015
+# - 138K Total Transactions, 40K Seed transactions, 98K Venture transactions
+
+from analysis_functions_similarity import *
+df = pd.read_csv('/Users/brendanwong/galvanize/Capstone/crunchbase-data/investments.csv')
+df = df_preprocessing(df)
+mask_d = (~df['funded_year_month'].isin(['2015-07', '2015-08', '2015-09', '2015-10', '2015-11', '2015-12']))
+observation_data = df[mask_d]
+len(observation_data)
+seed = observation_data[(observation_data['funding_round_type'] == 'venture')]
+seed = seed[(seed['company_country_code'] == 'FRA')]
+len(seed)
+seed_g = seed.groupby('investor_name').count().sort_values("company_name", ascending=False)
+
+hits = sum(seed_g['company_name'].iloc[0:10])
+hits
+hits
+seed_g['company_name'].iloc[0:10]
+
+
+
+def country_count():
+    countries = seed['investor_country_code'].unique()
+    seed_c = seed.groupby('company_country_code').count().sort_values("company_name", ascending=False)
+    seed_c
+
+
+test_data = observation_data
+len(test_data)
+test_data.to_csv('/Users/brendanwong/galvanize/interaction_data/delete2_data.csv')
+sf = graphlab.SFrame.read_csv('/Users/brendanwong/galvanize/interaction_data/delete2_data.csv')
+train, test = graphlab.recommender.util.random_split_by_user(sf, user_id="company_name", item_id="investor_name", max_num_users=25000)
+train_df = train.to_dataframe()
+train_df = train_df[(train_df['funding_round_type'] == 'venture')]
+train_list = train_df.groupby('investor_name').count().sort_values("company_name", ascending=False).reset_index()['investor_name'].iloc[0:10]
+len(train_df)
+train_list
+
+test_df = test.to_dataframe()
+test_df = test_df[(test_df['funding_round_type'] == 'venture')]
+
+len(test_df)
+sum(test_df['company_name'])
+test_df.groupby('investor_name').count().sort_values("company_name", ascending=False)
+
+test_list
--- run_similarity.py
+++ run_similarity.py
@@ -26,30 +26,33 @@ observation_data = df[mask_d]
 # This sparse interaction data would be expected to hurt the performance of my recommendation model.
 mask_i = (observation_data.groupby('investor_name')['company_name'].apply(lambda g: len(g) >50))
 observation_data_large = observation_data[(observation_data['investor_name'].isin(mask_i[mask_i].index))]
+len(observation_data_large)
 
-
-# # Model 1: Only using interaction data, item-similarity model, Jaccard difference
+# # Model 1: Only using interaction data, item-similarity model, Jaccard difference, 50+ investors
 interaction_data = observation_data_large[['company_name', 'investor_name']]
 interaction_data.to_csv('/Users/brendanwong/galvanize/interaction_data/interaction_data.csv')
 sf = graphlab.SFrame.read_csv('/Users/brendanwong/galvanize/interaction_data/interaction_data.csv')
-train, test = graphlab.recommender.util.random_split_by_user(sf, user_id="company_name", item_id="investor_name")
+train, test = graphlab.recommender.util.random_split_by_user(sf, user_id="company_name", item_id="investor_name", max_num_users=25000)
 m1 = graphlab.recommender.item_similarity_recommender.create(observation_data=train, user_id = 'company_name', item_id = 'investor_name')
-eval_1 = m1.evaluate(test, cutoffs=[40])
+eval_1 = m1.evaluate(test2, cutoffs=[40])
+eval_1
 
-sf = graphlab.SFrame({'X1':[1], 'company_name':["testco"], "investor_name":['Accel']})
-m1.recommend(users=["testco"], new_observation_data=sf)
-m1.get_current_options()
-m1.get_similar_items()
 
-# m1.list_fields()
-# m1.get_current_options()
-# m1.get_similar_items().print_rows(50)
-# m1.evaluate(sf, exclude_known_for_precision_recall=False)
-# m1.recommend(users=['23andMe'])
-folds = KFold(sf, num_folds=5)
-params = {'user_id':'company_name', 'item_id':'investor_name'}
-job = cross_val_score(folds, graphlab.recommender.item_similarity_recommender.create, params)
-job.get_results()
+# # Model 2: Same as model 1, but using all investors
+interaction_data2 = observation_data[['company_name', 'investor_name']]
+interaction_data2.to_csv('/Users/brendanwong/galvanize/interaction_data/interaction_data2.csv')
+sf2 = graphlab.SFrame.read_csv('/Users/brendanwong/galvanize/interaction_data/interaction_data2.csv')
+train2, test2 = graphlab.recommender.util.random_split_by_user(sf2, user_id="company_name", item_id="investor_name", max_num_users=25000)
+m2 = graphlab.recommender.item_similarity_recommender.create(observation_data=train2, user_id = 'company_name', item_id = 'investor_name')
+eval_2 = m2.evaluate(test2, cutoffs=[40])
+eval_2
+
+
+# # Cross Validation
+# folds = KFold(sf, num_folds=5)
+# params = {'user_id':'company_name', 'item_id':'investor_name'}
+# job = cross_val_score(folds, graphlab.recommender.item_similarity_recommender.create, params)
+# job.get_results()
 
 
 
